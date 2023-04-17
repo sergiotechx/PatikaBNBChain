4d408bb3efd811263456b10ca4ddc10143510cb3
Sergio Martinez <64911708+sergiotechx@users.noreply.github.com>
2023-04-17 00:56:39 -0500

    Add files via upload

---
+++ IVhardhatdeployTestnet/contracts/hello.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Hello{
+    function SayHello() pure public  returns(string memory){
+        return "Hello Patika";
+    }
+}
---
+++ IVhardhatdeployTestnet/hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  settings: {
+    optimizer: {
+      enabled: true
+    }
+  },
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [process.env.PRIVATE]
+    }
+  }
+};
